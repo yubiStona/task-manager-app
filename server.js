e0c9680ab7d087e1e6d0bad416588daa5cd59b31
Yubraj Dhungana <yubraj.dhungana21@gmail.com>
2025-04-15 11:44:30 +0545

    server file edited

--- server.js
+++ server.js
@@ -4,25 +4,27 @@ const authRoutes = require("./modules/auth/auth.routes");
 const userRoutes = require("./modules/users/user.routes");
 const taskRoutes = require("./modules/task/task.routes");
 const cors = require("cors");
-const csrf= require('csurf');
+const csrf = require("csurf");
 const cookieParser = require("cookie-parser");
 
 const app = express();
 
 //middleware
-app.use(cors({
-  origin:'http://localhost:5173',
-  credentials:true,
-  allowedHeaders:['Content-Type', 'Authorization', 'X-CSRF-Token'],
-  exposedHeaders:['set-cookie']
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization", "X-CSRF-Token"],
+    exposedHeaders: ["set-cookie"],
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 const csrfProtection = csrf({
   cookie: {
     httpOnly: false, // CSRF token must be accessible by JS
-    sameSite: 'Strict',
-  }
+    sameSite: "none",
+  },
 });
 app.use(csrfProtection);
 //mongoDB connection
@@ -31,8 +33,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
 
-app.get("/api/csrf-token",(req, res) => {
-  res.json({csrfToken:req.csrfToken()});
+app.get("/api/csrf-token", (req, res) => {
+  res.json({ csrfToken: req.csrfToken() });
 });
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
